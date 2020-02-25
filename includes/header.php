030ccaf3ca5bddde5a6d28e2071bad5109df382d
aidanshene <aidanshene@gmail.com>
2020-02-24 19:57:03 -0500

    fixed header.php paths

--- includes/header.php
+++ includes/header.php
@@ -4,14 +4,14 @@
     <!-- Aidan Shene -->
     <meta charset="utf-8">
     <title><?php echo $title ?></title>
-    <link rel="stylesheet" type="text/css" href="../styles/main.css" media="screen">
+    <link rel="stylesheet" type="text/css" href="styles/main.css" media="screen">
 </head>
 <body class="grid">
 <header class="navigation-sidebar-container">
     <nav class="navigation-sidebar">
         <ul class="navigation-sidebar-items">
-            <li><a class="logo" href="../index.php"></a></li>
-            <li><a href="../login.php">Login</a></li>
+            <li><a class="logo" href="index.php"></a></li>
+            <li><a href="login.php">Login</a></li>
             <li><a href="#">Contact</a></li>
         </ul>
     </nav>
